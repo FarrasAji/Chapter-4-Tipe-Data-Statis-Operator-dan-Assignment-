8f24fcc7f899f2f4bb77957c3526c17c9379b159
FarrasAji <Farrasaji.rpl2.16@gmail.com>
2020-10-18 00:31:48 +0700

    Add files via upload

---
+++ nomor 5 latihan chapter 4.py
@@ -0,0 +1,9 @@
+#Program menghitung jumlah mahasiswa
+simbol = '*'
+laki = int(input("jumlah mahasiswa laki-laki :"))
+hitung = laki // 10
+print("Laki-laki : ", simbol* hitung,laki)
+perempuan = int(input("jumlah mahasiswa perempuan :"))
+hitung2 = perempuan // 10
+print("Perempuan : ", simbol * hitung2,perempuan)
+
