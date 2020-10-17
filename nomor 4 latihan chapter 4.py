1920c1254ee7e69182f905c98c08644f7f0faa58
FarrasAji <Farrasaji.rpl2.16@gmail.com>
2020-10-18 00:31:13 +0700

    Add files via upload

---
+++ nomor 4 latihan chapter 4.py
@@ -0,0 +1,12 @@
+jarakAB = 125
+V1 = 62
+jarakBC = 256
+V2 = 70
+berangkat = 6
+istirahat = 45 / 60
+waktu1 = jarakAB // V1
+waktu2 = jarakBC // V2
+totalwaktu = ( waktu1 + waktu2 )
+sampai = berangkat + totalwaktu
+
+print("Pak amir tiba di Kota C pada pukul",sampai)
